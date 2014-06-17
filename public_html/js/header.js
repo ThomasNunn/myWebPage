955e3224cd72d1655bc4108abaa405f19e922acb
ThomasNunn <tmnunn7@live.com>
2014-06-16 21:13:29 -0700

    Structure mostly complete. Need to add content pages.

---
+++ public_html/js/header.js
@@ -0,0 +1,21 @@
+
+document.write(
+"        <meta charset='utf-8'>"+
+"        <meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>"+
+"        <title></title>"+
+"        <meta name='description' content=''>"+
+"        <meta name='viewport' content='width=device-width, initial-scale=1'>"+
+
+"        <link rel='stylesheet' href='css/bootstrap.min.css'>"+
+"        <style>"+
+"            body {"+
+"                padding-top: 50px;"+
+"                padding-bottom: 20px;"+
+"            }"+
+"        </style>"+
+"        <link rel='stylesheet' href='css/bootstrap-theme.min.css'>"+
+"        <link rel='stylesheet' href='css/main.css'>"+
+"        <script src='http://code.jquery.com/jquery-2.1.1.js'></script>"+
+"        <script src='js/vendor/modernizr-2.6.2-respond-1.1.0.min.js'></script>"        
+        );
+
---
+++ public_html/js/nav.js
@@ -0,0 +1,39 @@
+
+document.write(
+"    <nav class='navbar navbar-inverse navbar-fixed-top navbar-custom' role='navigation'>"+
+"        <!-- Brand and toggle get grouped for better mobile display -->"+
+"        <div class='container'>"+
+"            <div class='navbar-header'>"+
+"                <button type='button' class='navbar-toggle' data-toggle='collapse'>"+
+"                    <span class='sr-only'>Toggle navigation</span>"+
+"                    <span class='icon-bar'></span>"+
+"                    <span class='icon-bar'></span>"+
+"                    <span class='icon-bar'></span>"+
+"                </button>"+
+"                <a class='navbar-brand' href='index.html'>Thomas-Nunn</a>"+
+"            </div>"+
+"            <!-- Collect the nav links, forms, and other content for toggling -->"+
+"            <div class='collapse navbar-collapse'>"+
+"                <ul class='nav navbar-nav navbar-right'>"+
+"                    <li><a href='#'>Home</a></li>"+
+"                    <li><a href='#'>Profile</a></li>"+
+"                    <li><a href='#'>Contact Me</a></li>"+
+"                    <li class='dropdown'>"+
+"                        <a href='projects.html' data-toggle='dropdown' class='dropdown-toggle'>Projects <b class='caret'></b></a>"+
+"                        <ul class='dropdown-menu'>"+
+"                            <li><a href='#'>Java </a></li>"+
+"                            <li><a href='#'>C# </a></li>"+
+"                            <li><a href='#'>Javascript </a></li>"+
+"                            <li class='divider'></li>"+
+"                            <li><a href='#'>Other </a></li>"+
+"                        </ul>"+
+"                    </li>"+
+"                    <li><a href='#'>Knowledge</a></li>"+
+"                    <li><a href='#'>Education</a></li>"+
+"                </ul>"+
+"            </div><!-- /.navbar-collapse -->"+
+"        </div>"+
+"    </nav>"
+    );
+
+
